9ef1a103fa6446f5b2635d5dd12f0e278fc221af
David Smith <21258602+dewaynesmith3711@users.noreply.github.com>
2023-03-17 14:29:22 -0400

    Initialize test/staging/static

---
+++ .percy.js
@@ -0,0 +1,38 @@
+const SNAPSHOTS = [
+  '/en/index.html',
+  '/en/content-types/blog-landing/index.html',
+  '/en/content-types/docs-landing/index.html',
+  '/en/content-types/doc-post/index.html',
+  '/en/content-types/landing/index.html',
+];
+
+module.exports = {
+  version: 2,
+  snapshot: {
+    widths: [
+      375,
+      865,
+      1280,
+      1600
+    ],
+    percyCSS: '\n' +
+      '      iframe, .cookie-banner {\n' +
+      '        display: none !important;\n' +
+      '      }\n' +
+      '    ',
+    enableJavaScript: true
+  },
+  discovery: {
+    disableCache: true,
+    networkIdleTimeout: 250,
+    concurrency: 15,
+  },
+  static: {
+    baseUrl: '/',
+    include: SNAPSHOTS,
+    exclude: [
+      // Prevent percy to snapshot all index.html files in ./dist dir
+      ({ name }) => !SNAPSHOTS.includes(name)
+    ]
+  }
+}
