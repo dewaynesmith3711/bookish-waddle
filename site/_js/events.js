6a1b5adb3a81bdd9da5b6c018c39d31e1f62259c
David Smith <21258602+dewaynesmith3711@users.noreply.github.com>
2023-03-17 14:31:35 -0400

    Initialize jr-event-filters

---
+++ site/_js/events.js
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import './web-components/enhanced-event-card';
+import './web-components/truncate-text';
+import './web-components/enhanced-select';
+import './web-components/checkbox-group';
+// eslint-disable-next-line no-unused-vars
+import {EnhancedEventsList} from './web-components/enhanced-events-list';
+// eslint-disable-next-line no-unused-vars
+import {TagPillList} from './web-components/tag-pill-list';
+import {EnhancedSelect} from './web-components/enhanced-select';
+
+let activeFilters = {};
+/** @type {TagPillList|null} */
+const activeFiltersList = document.querySelector('#active-filters');
+/** @type {EnhancedEventsList|null} */
+const upcomingEvents = document.querySelector('#upcoming-events');
+/** @type {EnhancedEventsList|null} */
+const pastEvents = document.querySelector('#past-events');
+const selectFields = document.querySelectorAll('.events-filter');
+
+(() => {
+  selectFields.forEach(ele => {
+    ele.addEventListener('change', e => {
+      const t = e.target;
+
+      if (!(t instanceof EnhancedSelect)) {
+        return;
+      }
+
+      reactivelySetFilter(t.name, t.value);
+
+      injectFilters();
+    });
+  });
+
+  addMobileListeners();
+  handleDeselections();
+})();
+
+function injectFilters() {
+  if (upcomingEvents) upcomingEvents.filters = activeFilters;
+  if (pastEvents) pastEvents.filters = activeFilters;
+
+  if (activeFiltersList)
+    activeFiltersList.items = Object.entries(activeFilters).flatMap(i => {
+      return i[1].map(value => ({key: i[0], value: value}));
+    });
+}
+
+function addMobileListeners() {
+  /** @type {HTMLDialogElement|null} */
+  const filters = document.querySelector('#mobile-filters');
+  const opener = document.querySelector('#mobile-filters-opener');
+  const done = document.getElementById('mobile-filters-done');
+  const reset = document.getElementById('mobile-filters-reset');
+
+  // @ts-ignore
+  opener?.addEventListener('click', () => filters.showModal());
+
+  filters?.addEventListener('click', e => {
+    if (/** @type {HTMLElement} */ (e.target).nodeName === 'DIALOG')
+      closeFiltersModal();
+  });
+
+  done?.addEventListener('click', () => {
+    const selected = Array.from(
+      document.querySelectorAll(
+        '#mobile-filters input[type="checkbox"]:checked'
+      )
+    );
+
+    activeFilters = selected.reduce((target, checkbox) => {
+      const name = checkbox.getAttribute('name');
+      const value = checkbox.getAttribute('value');
+
+      if (name === null) return target;
+
+      if (typeof target[name] === 'undefined') {
+        target[name] = [value];
+        return target;
+      }
+
+      target[name].push(value);
+
+      return target;
+    }, {});
+
+    injectFilters();
+    closeFiltersModal();
+  });
+
+  reset?.addEventListener('click', () => {
+    document
+      .querySelectorAll('#mobile-filters input[type="checkbox"]:checked')
+      .forEach(checkbox => {
+        /** @type {HTMLInputElement } */ (checkbox).checked = false;
+      });
+  });
+
+  function closeFiltersModal() {
+    // @ts-ignore
+    filters?.close();
+  }
+}
+
+function handleDeselections() {
+  if (!activeFiltersList) return;
+
+  activeFiltersList.addEventListener('click', e => {
+    const target = e.target;
+
+    if (!(target instanceof HTMLElement)) return;
+
+    if (!target.matches('.tag-pill')) return;
+
+    const key = target.dataset.key;
+
+    reactivelySetFilter(
+      key,
+      activeFilters[key].filter(i => i !== target.dataset.value)
+    );
+
+    injectFilters();
+
+    selectFields.forEach(field => {
+      /** @type {EnhancedSelect} */ (field).setValue(
+        activeFilters[field.getAttribute('name')] || []
+      );
+    });
+
+    document
+      .querySelectorAll('#mobile-filters input[type="checkbox"]:checked')
+      .forEach(checkbox => {
+        const target = activeFilters?.[checkbox.getAttribute('name')];
+
+        /** @type {HTMLInputElement } */ (checkbox).checked =
+          target && target.includes(checkbox.getAttribute('value'));
+      });
+  });
+}
+
+function reactivelySetFilter(key, value) {
+  activeFilters = {
+    ...activeFilters,
+    ...{[key]: value},
+  };
+}
---
+++ site/_js/web-components/enhanced-events-list.js
@@ -0,0 +1,196 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Enhances events lists - reloading when filters
+ * applied and making it possible to fetch more.
+ */
+import {BaseElement} from './base-element';
+import {html} from 'lit-element';
+import memoize from '../utils/memoize';
+import {loadMore} from '../misc/load-more';
+import {unsafeHTML} from 'lit-html/directives/unsafe-html';
+
+const getEvents = memoize(async () => {
+  const response = await fetch('/events.json');
+
+  //todo - handle
+  if (response.status !== 200) {
+    throw new Error('Unable to fetch /events.json');
+  }
+
+  const all = await response.json();
+
+  return {
+    upcomingEvents: all.filter(event => !event.isPastEvent),
+    pastEvents: all.filter(event => event.isPastEvent),
+  };
+});
+
+export class EnhancedEventsList extends BaseElement {
+  constructor() {
+    super();
+
+    this.type = null;
+    this.filters = {};
+    this.total = null;
+    this.loader = null;
+    this.loadMoreButton = this.getLoadMoreButton();
+    this.loadedItems = [];
+    this.initialItems = [];
+    this.omitInitialItems = false;
+  }
+
+  static get properties() {
+    return {
+      filters: {type: Object, reflect: true},
+      type: {type: String, reflect: true},
+      loadedItems: {type: Array, reflect: false},
+      total: {type: Number, reflect: false},
+    };
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+
+    this.initialItems = Array.from(
+      this.querySelectorAll('enhanced-event-card')
+    );
+
+    this.loader = await this.initLoadMore();
+
+    this.loadMoreButton?.handleConnect();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    this.loadMoreButton?.handleDisconnect();
+  }
+
+  async attributeChangedCallback(name, oldval, newval) {
+    super.attributeChangedCallback(name, oldval, newval);
+
+    if ('resolved' in this.dataset && name === 'filters') {
+      this.omitInitialItems = true;
+
+      this.loadedItems = await this.loader?.restart();
+    }
+  }
+
+  getLoadMoreButton() {
+    const element = this.querySelector('.load-more__button');
+
+    const handleClick = async () => {
+      if (!element) return;
+
+      element.setAttribute('disabled', '');
+
+      const loaded = await this.loader?.nextPage();
+
+      this.loadedItems = this.loadedItems.concat(loaded || []);
+
+      element.removeAttribute('disabled');
+    };
+
+    return {
+      element,
+      handleConnect: () => {
+        element?.addEventListener('click', handleClick);
+      },
+      handleDisconnect: () => {
+        element?.removeEventListener('click', handleClick);
+      },
+    };
+  }
+
+  /**
+   * @param {{location:array, topics:array, googlers:array}[]} events
+   * @returns {*}
+   */
+  filterEvents(events) {
+    return events.filter(event => {
+      if (
+        this.filters.locations?.length &&
+        !this.filters.locations.some(location => location === event.location)
+      ) {
+        return false;
+      }
+
+      if (
+        this.filters.topics?.length &&
+        !this.filters.topics.some(topic => event.topics.includes(topic))
+      ) {
+        return false;
+      }
+
+      if (
+        this.filters.googlers?.length &&
+        !this.filters.googlers.some(googler => event.googlers.includes(googler))
+      ) {
+        return false;
+      }
+
+      return true;
+    });
+  }
+
+  /**
+   * @returns {Promise<{currentOffset, total, take, nextPage, isLastPage, restart}>}
+   */
+  async initLoadMore() {
+    return loadMore(
+      async (skip, take) => {
+        const groups = await getEvents();
+        const events =
+          this.type === 'UPCOMING'
+            ? this.filterEvents(groups.upcomingEvents)
+            : this.filterEvents(groups.pastEvents);
+
+        return {
+          updated_total: events.length,
+          items: events.slice(skip, take + skip).map(event => event.html),
+        };
+      },
+      () => {
+        document
+          .getElementById('loading-error')
+          ?.classList.remove('display-none');
+      },
+      {
+        skip: this.initialItems.length,
+        take: 10,
+        total: this.total,
+      }
+    );
+  }
+
+  render() {
+    const initialItems = this.omitInitialItems ? null : this.initialItems;
+    const loadMoreButton = !this.loader?.isLastPage()
+      ? this.loadMoreButton?.element
+      : null;
+
+    return html`
+      <div class="display-grid grid-cols-1 grid-gap-400">
+        ${initialItems} ${unsafeHTML(this.loadedItems.join(''))}
+      </div>
+      ${loadMoreButton}
+    `;
+  }
+}
+
+customElements.define('enhanced-events-list', EnhancedEventsList);
